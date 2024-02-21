0b11bc7d78b2aa733cdacd61b97ea2966af4ff09
Jamshid Isoqov <isoqovjamshid01@gmail.com>
2024-02-21 23:57:07 +0500

    modularization finished

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,16 @@ dependencyResolutionManagement {
 
 rootProject.name = "UniShop"
 include(":app")
- 
+include(":features:registration")
+include(":features:login")
+include(":features:home")
+include(":features:finance")
+include(":features:order")
+include(":features:clients")
+include(":features:warehouse")
+include(":features:debt")
+include(":data")
+include(":domain")
+include(":library:framework")
+include(":library:core-ui")
+include(":library:core-ui-resources")
